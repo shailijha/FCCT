f88f9b554badfc785c936f3e39e8db63cd3d4baa
shailijha <shaili_jha@yahoo.co.in>
2020-01-19 15:52:54 -0500

    steam roller prog

---
+++ steamroller.js
@@ -0,0 +1,15 @@
+function steamrollArray(arr) {
+  // I'm a steamroller, baby
+  var result = [];
+  for (let index = 0; index < arr.length; index++) {
+    if(Array.isArray(array[index]))
+    {
+      while(!)
+    }
+
+  }
+  console.log(...arr);
+  //return arr;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
